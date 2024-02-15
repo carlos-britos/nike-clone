e7670f7a1bdfeb06242a623c4a2f1e50dfbbeed1
Carlos <carlos.britos@web-experto.com>
2024-02-15 07:03:55 -0300

    Add header desktop

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -7,6 +7,7 @@ import { Logo } from '../icons/Logo';
 import { MenuBurgerButton } from '../icons/MenuBurgerButton';
 import { Search } from '../icons/Search';
 import { ShoppingBag } from '../icons/ShoppingBag';
+import { JordanLogo } from '../icons/JordanLogo';
 
 
 const Header = () => {
@@ -46,6 +47,16 @@ const Header = () => {
 
   return (
     <header className="header">
+      <div className="header__top-header">
+        <div className="top-header__logo">
+          <Icon iconSvg={<JordanLogo />} />
+        </div>
+        <div className="top-header__actions">
+          <div className="">Buscar tienda</div>
+          <div className="">Ayuda</div>
+        </div>
+        
+      </div>
       <div className="header__wrapper">
         {/* {!isDesktop && <MenuMobile darkMode={ darkMode } toggleThemeChange={ toggleThemeChange } />} */}
 
@@ -57,9 +68,19 @@ const Header = () => {
           <Icon iconSvg={<Logo />} />
         </Link>
 
+        <div className="header__categories">
+          <div className="categories__item">Destacados</div>
+          <div className="categories__item">Hombre</div>
+          <div className="categories__item">Mujer</div>
+          <div className="categories__item">Niño/a</div>
+          <div className="categories__item">Accesorios</div>
+          <div className="categories__item">Sale</div>
+        </div>
+
         <div className="header__actions">
           <div className="actions__search">
             <Icon iconSvg={<Search />} />
+            <input type="text" placeholder="Buscar" />
           </div>
           <div className="actions__cart">
             <Icon iconSvg={<ShoppingBag />} />
