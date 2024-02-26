2f9be47ffa65500e543b42c2c89c48df14cc0ee1
Carlos <carlos.britos@web-experto.com>
2024-02-26 02:58:43 -0300

    Fix home reordened

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import './App.scss'
+import { useEffect, useState } from "react";
 import { Header } from './components/shared/Header'
 import { CarouselInfo } from './components/views/home/CarouselInfo'
 import { BannerHome } from './components/views/home/BannerHome'
 import { ProductCarousel } from './components/shared/ProductCarousel'
-import { BannerHomeCategory } from './components/views/home/BannerHomeCategory'
-import { CategoryCarousel } from './components/shared/CategoryCarousel'
+import { DobleBannerHomeCategory } from './components/views/home/DobleBannerHomeCategory'
+import { FeaturedCategories } from './components/views/home/FeaturedCategories'
+import { CategoryCarousel } from './components/shared/CategoryCarousel';
 import { Footer } from './components/shared/Footer'
 
 function App() {
+  const [isDesktop, setIsDesktop] = useState(window.innerWidth > 991);
+
+  useEffect(() => {
+    const handleResize = () => {
+        setIsDesktop(window.innerWidth > 991);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <>
       <Header />
       <CarouselInfo />
       <BannerHome />
       <ProductCarousel />
-      <BannerHomeCategory />
-      <BannerHomeCategory />
+      <DobleBannerHomeCategory />
       <ProductCarousel />
-      <CategoryCarousel />
-      <CategoryCarousel />
-      <BannerHome />
+      <FeaturedCategories isDesktop={isDesktop} />
+      <CategoryCarousel/>
+      <BannerHome classes="mt-3-rem" />
       <Footer />
     </>
   )
